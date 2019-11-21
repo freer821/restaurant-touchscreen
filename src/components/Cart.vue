b3326f853d3a3c5234c22bafe1eaa6b5ecf0cb3e
ZhenyuGeng <freergzy8212@hotmail.com>
2019-11-21 22:52:23 +0100

    add modal to checkout

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -6,10 +6,10 @@
                 Shipping cart
             </div>
             <div class="card-body">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
 
             </div>
             <div class="card-footer">
@@ -24,21 +24,114 @@
                     </div>
                 </div>
                 <div class="float-right" style="margin: 10px">
-                    <a href="" class="btn btn-success float-right">Checkout</a>
                     <div class="float-right" style="margin: 5px">
                         Total price: <b>50.00€</b>
                     </div>
+                    <b-button @click="show=true" variant="primary">Checkout</b-button>
                 </div>
             </div>
         </div>
+        <b-modal
+                v-model="show"
+                title="Modal Variants"
+                :header-bg-variant="headerBgVariant"
+                :header-text-variant="headerTextVariant"
+                :body-bg-variant="bodyBgVariant"
+                :body-text-variant="bodyTextVariant"
+                :footer-bg-variant="footerBgVariant"
+                :footer-text-variant="footerTextVariant"
+        >
+            <b-container fluid>
+                <b-row class="mb-1 text-center">
+                    <b-col cols="3"></b-col>
+                    <b-col>Background</b-col>
+                    <b-col>Text</b-col>
+                </b-row>
+
+                <b-row class="mb-1">
+                    <b-col cols="3">Header</b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="headerBgVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="headerTextVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                </b-row>
+
+                <b-row class="mb-1">
+                    <b-col cols="3">Body</b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="bodyBgVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="bodyTextVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                </b-row>
+
+                <b-row>
+                    <b-col cols="3">Footer</b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="footerBgVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                    <b-col>
+                        <b-form-select
+                                v-model="footerTextVariant"
+                                :options="variants"
+                        ></b-form-select>
+                    </b-col>
+                </b-row>
+            </b-container>
+
+            <template v-slot:modal-footer>
+                <div class="w-100">
+                    <p class="float-left">Modal Footer Content</p>
+                    <b-button
+                            variant="primary"
+                            size="sm"
+                            class="float-right"
+                            @click="show=false"
+                    >
+                        Close
+                    </b-button>
+                </div>
+            </template>
+        </b-modal>
     </div>
 </template>
 
 <script>
     import Product from "./Product";
+
     export default {
         name: "Cart",
-        components:{ Product }
+        components: {Product},
+        data() {
+            return {
+                show: false,
+                variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
+                headerBgVariant: 'dark',
+                headerTextVariant: 'light',
+                bodyBgVariant: 'light',
+                bodyTextVariant: 'dark',
+                footerBgVariant: 'warning',
+                footerTextVariant: 'dark'
+            }
+        }
     }
 </script>
 
@@ -46,10 +139,10 @@
     .card-header {
         margin: 0px;
     }
+
     .shopping-cart {
         margin-top: 20px;
     }
-
     .card-body {
         margin: 0px;
         overflow-y: auto;
@@ -57,3 +150,4 @@
     }
 
 </style>
+
