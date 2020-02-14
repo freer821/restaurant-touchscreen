280a6b56c310b11fe86f716bad089df738ae9e14
ZhenyuGeng <freergzy8212@hotmail.com>
2020-02-14 11:49:37 +0100

    update

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -20,14 +20,20 @@
 <style lang="scss" scoped>
     .app-main {
         /* 50= navbar  50  */
-        min-height: calc(100vh - #{$navBarHeight});
+    /*min-height: calc(100vh - #{$navBarHeight}); */
+        height: 100%;
         width: 100%;
         position: relative;
         overflow: hidden;
-    }
+        background: url('~@/assets/images/back.jpg') no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+}
 
-    .fixed-header + .app-main {
-        padding-top: #{$navBarHeight};
-    }
+.fixed-header + .app-main {
+    padding-top: #{$navBarHeight};
+}
 
 </style>
--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="navbar">
-        hallo navbar
+        <div style="padding: 20px">
+            <el-row :gutter="20">
+                <el-col :span="6"> <img src="~@/assets/images/g1.png" ></el-col>
+                <el-col :span="6"><img src="~@/assets/images/g2.png" ></el-col>
+                <el-col :span="6"><img src="~@/assets/images/g3.png" ></el-col>
+                <el-col :span="6"><img src="~@/assets/images/g4.png" ></el-col>
+            </el-row>
+        </div>
     </div>
 </template>
 
@@ -15,7 +22,10 @@
         height: #{$navBarHeight};
         overflow: hidden;
         position: relative;
-        background: #fff;
+        background-color: #2e130a;
         box-shadow: 0 1px 4px rgba(0,21,41,.08);
+    }
+    .el-col {
+        text-align:center;
     }
 </style>
